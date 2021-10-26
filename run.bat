2072fd60dc05bc40fb1ec9b4b664d26240ce09e0
sridevi <sridevi@gmail.com>
2021-10-26 11:00:28 -0500

    it is my first commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\sridevigade\eclipse-workspace\opencartV001
+mvn clean test
---
+++ src/test/java/pageObjects/HomePage.java
@@ -0,0 +1,45 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage
+{
+	WebDriver driver;
+	
+	//Constructor
+	public HomePage(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Locators
+	@FindBy(xpath="//span[text()='My Account']")
+	WebElement lnkMyaccount;
+	
+	@FindBy(linkText="Register")
+	WebElement lnkRegister;
+	
+	
+	@FindBy(linkText="Login")
+	WebElement lnkLogin;
+	
+	//Actions
+	public void clickMyAccount()
+	{
+		lnkMyaccount.click();
+	}
+	
+	public void clickRegister()
+	{
+		lnkRegister.click();
+	}
+	
+	public void clickLogin()
+	{
+		lnkLogin.click();
+	}
+}
